0c29902490863bb9dc263740368c3964bf8726df
imadiaz <imanueld22@gmail.com>
2020-10-27 11:50:24 -0600

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Immanuel Díaz on 27/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
